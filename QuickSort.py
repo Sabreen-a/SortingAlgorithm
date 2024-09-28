259e7bc82bb77970fd701876466ce185ff4d1ce6
Sabreen <83454541+Sabreen-a@users.noreply.github.com>
2024-09-28 12:56:49 -0700

    add mprof plot

--- QuickSort.py
+++ QuickSort.py
@@ -5,7 +5,11 @@ def QuickSort(arr):
     pivot = arr[0]
     smaller = [i for i in arr[1:] if i <= pivot]
     greater = [i for i in arr[1:] if i > pivot]  
-    print(f'Pivot: {pivot}, Smaller: {smaller}, Greater: {greater}') 
+    #print(f'Pivot: {pivot}, Smaller: {smaller}, Greater: {greater}') 
+    print(QuickSort(smaller) + [pivot] + QuickSort(greater)) 
     return QuickSort(smaller) + [pivot] + QuickSort(greater)
 
 print(QuickSort([10, 2, 3, 4, 5]))
+
+#https://jamesg.blog/2024/06/16/python-memory-usage/ for visualize time complexity >mprof run QuickSort.py mprof plot
+
